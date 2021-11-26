9c8a9f8e19f96d5f68b1adce3a5df83a4e595b0d
daniel.bustos <daniel.bustos@mundopacifico.cl>
2021-11-26 09:09:35 -0300

    update

--- index.js
+++ index.js
@@ -21,10 +21,13 @@ stocknew.prices = stockRegisterList
 let fecha_ini =  "2018-04-30"
 let fecha_fin =  "2019-06-30"
 let porfalolionew = new portafolio(fecha_ini,fecha_fin,stocknew)
+//Rentabilidad simple sacara la rentabilidad entre 2 fechas
 
 console.log("RENTABILIDAD SIMPLE")
 console.log(porfalolionew.profit("SIMPLE"))
 
 console.log("RENTABILIDAD ANUALIZADA")
 console.log(porfalolionew.profit("ANUALIZADA"))
-
+//Rentabilidad anualizada, segun yo hay que sacar la rentabilidad entre el primer y ultimo dia de cada año ingresado 
+//Sumarlo y sacar la raiz de n, donde n es el numero de años que estamos calculando pero puedo estar 
+//equivocado XD
